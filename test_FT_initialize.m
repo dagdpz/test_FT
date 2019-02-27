740b0e96ab834ef70c24b69bb77a9ec50f2563b1
Igor Kagan <abderian.affe@gmail.com>
2019-02-27 18:44:55 +0100

    test_FT_initialize fix

--- test_FT_initialize.m
+++ test_FT_initialize.m
@@ -2,9 +2,10 @@ addpath('Y:\Sources\fieldtrip-20190130'); % or other path to fieldtrip
 ft_defaults
 
 % http://www.fieldtriptoolbox.org/faq/can_i_prevent_external_toolboxes_from_being_added_to_my_matlab_path/
+% (fixed version from above)
 [ftver, ftpath] = ft_version;
-rmpath(fullfile(ftver, 'external', 'signal'))
-rmpath(fullfile(ftver, 'external', 'stats'))
-rmpath(fullfile(ftver, 'external', 'image'))
+rmpath(fullfile(ftpath, 'external', 'signal'))
+rmpath(fullfile(ftpath, 'external', 'stats'))
+rmpath(fullfile(ftpath, 'external', 'images'))
 
 % addpath(genpath('Y:\Sources\chronux_2_12')) % chronux with its subfolders
